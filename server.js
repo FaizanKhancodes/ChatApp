47b74ddc8f5db3ae4efc5c7514d1e5b6a4eb106c
FaizanKhancodes <faiazniclouds@gmail.com>
2025-11-08 13:52:44 +0530

    Prepare for Render Deployment

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ function broadcastUserList() {
   });
 }
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () =>
   console.log(`✅ Server running on http://localhost:${PORT}`)
 );
